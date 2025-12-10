ec035c2c5d931553708276a42d93db253d2fdad4
Dani Rogmans <danirogmans@gmail.com>
2025-12-10 14:30:29 +0100

    try

--- src/calculator.py
+++ src/calculator.py
@@ -1,6 +1,6 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    def add(self, a, b, c):
+        return a + b + c
 
-    def multiply(self, a, b):
-        return a * b
+    def multiply(self, a, b, c):
+        return a * b * c
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,8 +2,8 @@ from src.calculator import Calculator
 
 def test_add():
     calc = Calculator()
-    assert calc.add(2, 3) == 5
+    assert calc.add(2, 3, 0) == 5
 
 def test_multiply():
     calc = Calculator()
-    assert calc.multiply(4, 5) == 20
+    assert calc.multiply(4, 5, 1) == 20
